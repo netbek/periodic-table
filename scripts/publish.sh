75fba03e9caa31e3698b9d86a9bf988934e56129
Hein Bekker <hein@netbek.co.za>
2024-06-15 03:42:31 +0200

    Add version arg

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 set -e
 
+if ([ "$1" == "--help" ] || [ -z "$1" ]); then
+    echo "Usage: publish.sh [version]"
+    echo ""
+    echo "Example: ./publish.sh 0.0.1"
+    exit 1
+fi
+
 npm publish
 
 python3 -m twine upload --config-file .pypirc --verbose dist/*
 
-gh release create [VERSION]
+gh release create $1
