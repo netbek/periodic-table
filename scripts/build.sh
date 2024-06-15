808e893582026cc4c1107a4fbffa1ad00f2bfd91
Hein Bekker <hein@netbek.co.za>
2024-06-15 12:21:07 +0200

    Delete old artifacts before build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e
 
+scripts_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+repo_dir="${scripts_dir}/.."
+
+find "${repo_dir}/dist" -maxdepth 1 ! -name ".gitignore" -type f -exec rm -f {} +
 python3 -m build
