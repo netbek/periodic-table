4dcd9deb9c2a8c502d0a7d611995fc635d8e8737
Hein Bekker <hein@netbek.co.za>
2024-06-16 10:27:18 +0200

    Add folder for Python src and dist

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,6 +3,8 @@ set -e
 
 scripts_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 repo_dir="${scripts_dir}/.."
+python_dist_dir="${repo_dir}/python/dist"
 
-find "${repo_dir}/dist" -maxdepth 1 ! -name ".gitignore" -type f -exec rm -f {} +
-python3 -m build
+gulp
+find "${python_dist_dir}" -maxdepth 1 ! -name ".gitignore" -type f -exec rm -f {} +
+python3 -m build -o "${python_dist_dir}"
