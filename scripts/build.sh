674208ab34b97a4f7bd5d4961a73f68a858eb8ec
Hein Bekker <hein@netbek.co.za>
2025-10-10 20:01:36 +0200

    Drop support for Python 3.9

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,9 +3,10 @@ set -e
 
 scripts_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 repo_dir="${scripts_dir}/.."
-python_dist_dir="${repo_dir}/python/dist"
 
-venv/bin/python python/dump.py
+cd "${repo_dir}"
+.venv/bin/python python/dump.py
 gulp
-find "${python_dist_dir}" -maxdepth 1 ! -name ".gitignore" -type f -exec rm -f {} +
-python3 -m build -o "${python_dist_dir}"
+find python/dist -maxdepth 1 ! -name ".gitignore" -type f -exec rm -f {} +
+uv build -o python/dist
+.venv/bin/twine check python/dist/*
--- scripts/install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -e
-
-virtualenv --python=python3.8 venv
-venv/bin/pip install -r requirements_dev.txt
-
-npm ci
